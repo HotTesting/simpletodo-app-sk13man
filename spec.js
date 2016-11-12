0bd18da865d25439ea4d924f2bf3b699b98695a2
Sergey Golovin <s.golovin@trip-top.com>
2016-11-12 18:03:40 +0200

    my_first_commit

--- spec.js
+++ spec.js
@@ -4,4 +4,8 @@ describe('suite', function () {
         browser.get('http://maxcabrera.com/code/todo-list/')
         expect(browser.getTitle()).toBe('Angular2 Seed')
     })
+    it('test2',funtion () {
+        browser.get('https://github.com/StartITProtractorJS')
+        expect(browser.getTitle()).toBe('StartITProtractorJS')
+    }
 })
