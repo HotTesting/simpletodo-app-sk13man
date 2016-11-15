4a8ea330435a3b202d16db5f4bf651af32f66c30
Sergey Golovin <s.golovin@trip-top.com>
2016-11-15 04:10:10 +0200

    Ne spitsa v 4 utra

--- spec.js
+++ spec.js
@@ -1,11 +1,36 @@
-describe('suite', function () {
+//Lets see what we have
 
+ let URL = 'http://maxcabrera.com/code/todo-list/'
+ let webelement = $('input.enter-todo')
+ let notes = $$('todo-list .small-12')
+ let checkboxes = element.all(by.css('.todo-container [type="checkbox"]'));
+
+ //
+describe('suite', function () {
+// Initial test
     it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
+        browser.get(URL)
         expect(browser.getTitle()).toBe('Angular2 Seed')
     })
-    it('test2', function () {
-        browser.get('https://github.com/StartITProtractorJS')
-        expect(browser.getTitle()).toBe('StartITProtractorJS')   
-})
+// Lets create a new note
+    it('should create new note', function () {
+        browser.get(URL)    
+        webelement.sendKeys('13')
+        browser.sleep(2000)
+        webelement.submit()
+     // Lets check created note 
+       expect(notes.getText()).toContain('13')
+    })
+  //Lets delete this notes
+    it('Lets delete this note', function() {
+        browser.get(URL)
+               element($("input[type='checkbox']").click())
+// Lets check this note is deleted
+expect(notes.getText()).not.toContain('13')
+    })
+// and lets delete them all
+  it('should delete all notes', function () {
+        checkboxes.click();
+        expect(checkboxes.count()).toBe(0);
+    });
 })
